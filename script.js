5a24e41d473a5a4497551a269f13541cd0f976df
Ritesh Kishor Chaudhari <100477678+Ritu0097@users.noreply.github.com>
2024-02-18 00:07:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,25 +1,12 @@
 function convertToRoman(num) {
-   const obj = {
-      0:['M',1000], 
-      1:['CM',900], 
-      2:['D', 500], 
-      3:['CD',400], 
-      4:['C', 100], 
-      5:['XC',90], 
-      6:['L', 50],
-      7:['XL',40], 
-      8:['X', 10],  
-      9:['IX', 9], // Change 'V' to 'IX'
-      10:['V',5],
-      11:['IV',4], 
-      12:['I', 1] // Add 'I'
-    };
-    let ans='';
-    for (let i=0;i<Object.keys(obj).length; i++) {
-        while (num >= obj[i][1]) {
-            ans += obj[i][0];
-            num -= obj[i][1];
-        }
-    }
-    return ans;
+   const romanNumerals = ['M','CM','D','CD','C','XC','L','XL','X','IX','V','IV','I'];
+   const values = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+   let ans = '';
+   for (let i = 0; i < values.length; i++) {
+      while (num >= values[i]) {
+         ans += romanNumerals[i];
+         num -= values[i];
+      }
+   }
+   return ans;
 }
